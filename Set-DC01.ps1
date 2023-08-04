d1f8eae5adacdba7314ceb66bc95033e6b1ffc3f
WodenSec <112494665+WodenSec@users.noreply.github.com>
2023-08-04 19:23:22 +0200

    Update Set-DC01.ps1

--- Set-DC01.ps1
+++ Set-DC01.ps1
@@ -36,10 +36,9 @@ function Nuke-Defender{
     Set-MpPreference -DisableRestorePoint  $true | Out-Null
     Set-MpPreference -DisableScanningMappedNetworkDrivesForFullScan  $true | Out-Null
     Set-MpPreference -DisableScanningNetworkFiles  $true | Out-Null
-    Set-MpPreference -DisableScriptScanning $true | Out-Null
 
     reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /f /v EnableLUA /t REG_DWORD /d 0 > $null
-    reg add "HKLM\System\CurrentControlSet\Services\SecurityHealthService" /v "Start" /t REG_DWORD /d "4" /f > $null
+    reg add "HKLM\System\CurrentControlSet\Services\SecurityHealthService" /v "Start" /t REG_DWORD /d "4" /f > $null   Set-MpPreference -DisableScriptScanning $true | Out-Nul
     reg add "HKLM\Software\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d "1" /f > $null
     reg add "HKLM\Software\Policies\Microsoft\Windows Defender" /v "DisableAntiVirus" /t REG_DWORD /d "1" /f > $null
     reg add "HKLM\Software\Policies\Microsoft\Windows Defender\MpEngine" /v "MpEnablePus" /t REG_DWORD /d "0" /f > $null
@@ -78,6 +77,14 @@ function Nuke-Defender{
     reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint" /v "NoWarningNoElevationOnInstall" /t REG_DWORD /d "1" /f > $null
     reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint" /v "RestrictDriverInstallationToAdministrators" /t REG_DWORD /d "0" /f > $null
 
+
+    Get-WmiObject -query "Select HotFixID  from Win32_QuickFixengineering" | sort-object -Descending -Property HotFixID|%{
+    $sUpdate=$_.HotFixID.Replace("KB","")
+    write-host ("Uninstalling update "+$sUpdate);
+    & wusa.exe /uninstall /KB:$sUpdate /quiet /norestart;
+    Wait-Process wusa 
+    Start-Sleep -s 1 }
+
 }
 
 function Get-QoL{
@@ -89,6 +96,25 @@ function Get-QoL{
     reg add  "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Control Panel\Desktop" /v "ScreenSaveTimeOut" /t REG_DWORD /d "0" /f > $null 
     reg add  "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Control Panel\Desktop" /v "ScreenSaveActive" /t REG_DWORD /d "0" /f > $null
     reg add  "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Control Panel\Desktop" /v "ScreenSaverIsSecure" /t REG_DWORD /d "0" /f > $null
+
+    Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask | Out-Null
+}
+
+function Build-Server{
+    write-host("`n  [++] Installing Module Active Directory Domain Services (ADDS)")
+    Install-windowsfeature -name AD-Domain-Services -IncludeManagementTools -WarningAction SilentlyContinue | Out-Null
+
+    write-host("`n  [++] Importing Module ActiveDirectory")
+    Import-Module ActiveDirectory -WarningAction SilentlyContinue | Out-Null
+
+    write-host("`n  [++] Installing ADDS Domain : wodensec.local ")
+    Install-ADDSDomain -SkipPreChecks -ParentDomainName WODENSEC -NewDomainName local -NewDomainNetbiosName WODENSEC -InstallDns -SafeModeAdministratorPassword (Convertto-SecureString -AsPlainText "R00tR00t" -Force) -Force -WarningAction SilentlyContinue | Out-Null
+
+    write-host("`n  [++] Deploying Active Directory Domain Forest in wodensec.local")
+    Install-ADDSForest -SkipPreChecks -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "WinThreshold" -DomainName "WODENSEC.local" -DomainNetbiosName "WODENSEC" -ForestMode "WinThreshold" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\Windows\SYSVOL" -Force:$true -SafeModeAdministratorPassword (Convertto-SecureString -AsPlainText "R00tR00t" -Force) -WarningAction SilentlyContinue | Out-Null
+
+
+
 }
 
 function Invoke-DCSetup{
@@ -102,8 +128,8 @@ function Invoke-DCSetup{
         Write-Host("Le serveur va être renommé puis redémarrer")
         Start-Sleep -Seconds 5
         Rename-Computer -NewName "DC01" -Restart
-    }else{
-        
-    
+    }elseif($env:USERDNSDOMAIN -ne "WODENSEC.LOCAL"){
+        Write-Host("Deuxième execution detectée. Installation des rôles...")
+        Build-Server
     }
 }
