4141e7c57a37099f9f36caa32bf380aeddd53d1f
WodenSec <112494665+WodenSec@users.noreply.github.com>
2023-12-22 16:21:36 +0100

    Update Set-PC02.ps1

--- Set-PC02.ps1
+++ Set-PC02.ps1
@@ -1,78 +1,6 @@
 #Requires -RunAsAdministrator
 
-function Nuke-Defender{
-    Set-MpPreference -DisableRealtimeMonitoring $true | Out-Null
-    Set-MpPreference -DisableRemovableDriveScanning $true | Out-Null
-    Set-MpPreference -DisableArchiveScanning  $true | Out-Null
-    Set-MpPreference -DisableAutoExclusions  $true | Out-Null
-    Set-MpPreference -DisableBehaviorMonitoring  $true | Out-Null
-    Set-MpPreference -DisableBlockAtFirstSeen $true | Out-Null
-    Set-MpPreference -DisableCatchupFullScan  $true | Out-Null
-    Set-MpPreference -DisableCatchupQuickScan $true | Out-Null
-    Set-MpPreference -DisableEmailScanning $true | Out-Null
-    Set-MpPreference -DisableIntrusionPreventionSystem  $true | Out-Null
-    Set-MpPreference -DisableIOAVProtection  $true | Out-Null
-    Set-MpPreference -DisablePrivacyMode  $true | Out-Null
-    Set-MpPreference -DisableRealtimeMonitoring  $true | Out-Null
-    Set-MpPreference -DisableRemovableDriveScanning  $true | Out-Null
-    Set-MpPreference -DisableRestorePoint  $true | Out-Null
-    Set-MpPreference -DisableScanningMappedNetworkDrivesForFullScan  $true | Out-Null
-    Set-MpPreference -DisableScanningNetworkFiles  $true | Out-Null
-    Set-MpPreference -DisableScriptScanning $true | Out-Null
-
-    reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /f /v EnableLUA /t REG_DWORD /d 0 > $null
-    reg add "HKLM\System\CurrentControlSet\Services\SecurityHealthService" /v "Start" /t REG_DWORD /d "4" /f > $null  
-    reg add "HKLM\Software\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d "1" /f > $null
-    reg add "HKLM\Software\Policies\Microsoft\Windows Defender" /v "DisableAntiVirus" /t REG_DWORD /d "1" /f > $null
-    reg add "HKLM\Software\Policies\Microsoft\Windows Defender\MpEngine" /v "MpEnablePus" /t REG_DWORD /d "0" /f > $null
-    reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableBehaviorMonitoring" /t REG_DWORD /d "1" /f > $null
-    reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableIOAVProtection" /t REG_DWORD /d "1" /f > $null
-    reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableOnAccessProtection" /t REG_DWORD /d "1" /f > $null
-    reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableRealtimeMonitoring" /t REG_DWORD /d "1" /f > $null
-    reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableScanOnRealtimeEnable" /t REG_DWORD /d "1" /f > $null
-    reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableScriptScanning" /t REG_DWORD /d "1" /f > $null 
-    reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Reporting" /v "DisableEnhancedNotifications" /t REG_DWORD /d "1" /f > $null
-    reg add "HKLM\Software\Policies\Microsoft\Windows Defender\SpyNet" /v "DisableBlockAtFirstSeen" /t REG_DWORD /d "1" /f > $null
-    reg add "HKLM\Software\Policies\Microsoft\Windows Defender\SpyNet" /v "SpynetReporting" /t REG_DWORD /d "0" /f > $null
-    reg add "HKLM\Software\Policies\Microsoft\Windows Defender\SpyNet" /v "SubmitSamplesConsent" /t REG_DWORD /d "2" /f > $null
-    reg add "HKLM\System\CurrentControlSet\Control\WMI\Autologger\DefenderApiLogger" /v "Start" /t REG_DWORD /d "0" /f > $null
-    reg add "HKLM\System\CurrentControlSet\Control\WMI\Autologger\DefenderAuditLogger" /v "Start" /t REG_DWORD /d "0" /f > $null
-
-    schtasks /Change /TN "Microsoft\Windows\ExploitGuard\ExploitGuard MDM policy Refresh" /Disable > $null
-    schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Disable > $null
-    schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Disable > $null
-    schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Disable > $null
-    schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Disable > $null
-
-    reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v "NoAutoUpdate" /t REG_DWORD /d "1" /f > $nul
-    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "LocalAccountTokenFilterPolicy" /t REG_DWORD /d "1" /f > $null
-
-    Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled False | Out-Null
-    Get-NetFirewallRule -Group '@FirewallAPI.dll,-32752'|Set-NetFirewallRule -Profile 'Private, Domain' -Enabled true -PassThru|select Name,DisplayName,Enabled,Profile |ft -a | Out-Null
-    netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=allow > $null
-    netsh advfirewall firewall add rule name="ICMP Allow incoming V6 echo request" protocol=icmpv6:8,any dir=in action=allow > $nul
-    Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled False | Out-Null
-    
-    # SMB signing enabled but not required
-    reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\LanmanWorkstation\Parameters" /v "RequireSecuritySignature" /t REG_DWORD /d "0" /f > $null
-    reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" /v "requiresecuritysignature" /t REG_DWORD /d "0" /f > $null
-    # PrintNightmare
-    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint" /v "NoWarningNoElevationOnInstall" /t REG_DWORD /d "1" /f > $null
-    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint" /v "RestrictDriverInstallationToAdministrators" /t REG_DWORD /d "0" /f > $null
-
-
-    Get-WmiObject -query "Select HotFixID  from Win32_QuickFixengineering" | sort-object -Descending -Property HotFixID|%{
-    $sUpdate=$_.HotFixID.Replace("KB","")
-    write-host ("Uninstalling update "+$sUpdate);
-    & wusa.exe /uninstall /KB:$sUpdate /quiet /norestart;
-    Wait-Process wusa 
-    Start-Sleep -s 1 }
-
-}
-
-
-function Set-PC02 { 
-
+function Invoke-PC02Setup { 
 
     if ($env:COMPUTERNAME -ne "PC02") { 
         write-host ("`n Changement des paramètres IP et du nom et reboot...")
@@ -89,15 +17,15 @@ function Set-PC02 {
 
     }
     elseif ($env:COMPUTERNAME -eq "PC02" -and $env:USERDNSDOMAIN -ne "WODENSEC.LOCAL") {
-        write-host ("`n Suppression de l'antivirus, ajout au domaine et reboot...")
+        write-host ("`n Ajout au domaine et reboot...")
         
-        Nuke-Defender
         $domain = "WODENSEC"
         $password = "R00tR00t" | ConvertTo-SecureString -asPlainText -Force
         $username = "$domain\Administrateur" 
         $credential = New-Object System.Management.Automation.PSCredential($username,$password)
         Add-Computer -DomainName $domain -Credential $credential  | Out-Null 
 
+        Sleep 5
         restart-computer
 
     }
